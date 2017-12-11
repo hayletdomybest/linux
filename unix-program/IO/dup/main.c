545f3cf0ae45b60dd2dabfd4782bb7209c74eef8
t628x7600 <33081637+t628x7600@users.noreply.github.com>
2017-12-11 15:38:05 +0800

    Add files via upload
     add dup example

---
+++ unix-program/IO/dup/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int error(char* s)
+{
+	fprintf(stderr,s);
+	return -1;
+}
+
+int main(int argc,char** argv)
+{
+	int fd=open("test",O_WRONLY|O_CREAT|O_APPEND,0666);
+	if(fd<0)
+		return error("fd open error\n");
+	int newfd=dup2(STDOUT_FILENO,fd);
+	if(newfd<0)
+		return error("newfd dup error\n");
+	printf("test\n");
+	return 0;
+}
