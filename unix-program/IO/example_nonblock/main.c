ca65afb13cb0d163a5c40a1c3a061a6da0989e05
user <user@user.(none)>
2017-12-13 22:56:15 +0800

    complete nonblock example

--- unix-program/IO/example_nonblock/main.c
+++ unix-program/IO/example_nonblock/main.c
@@ -4,9 +4,13 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <memory.h>
+#include <string.h>
+#define size 100000
 
-
-
+char buf[size];
+const char error_message[]="In %d time error %d\n";
+//char fmt[] = "%d Hi :( -> :) ---aha!---";
 int set_nonblock(int fildes,int status)
 {
 	int val=fcntl(fildes,F_GETFL);
@@ -37,15 +41,44 @@ int display_fl(int fildes)
 
 int main(int argc,char** argv)
 {
-	fprintf(stderr," stdout before set non block fl:\n");
-	display_fl(STDOUT_FILENO);
-	if(!(set_nonblock(STDOUT_FILENO,1)))
+	char wbuf[]="%d Hi :( -> :) ---aha!---\n";
+	
+	char rebuf[40];
+	int nbytes=0,wbytes=0,j=0,ntime=0,success=0,error=0;
+	int fd=open("file",O_WRONLY|O_CREAT|O_APPEND,0666);
+	memset(rebuf,0,40);
+	if((set_nonblock(STDOUT_FILENO,1))<0)
 	{
 		fprintf(stderr,"set non block error\n");
+		return -1;
 	}
-	fprintf(stderr," stdout after set non block fl:\n");
-	display_fl(STDOUT_FILENO);
-	
+	while((nbytes+sizeof(wbuf))<size)
+	{
+		sprintf(&buf[nbytes],wbuf,j++);
+		nbytes+=sizeof(wbuf);
+		buf[nbytes]='\n';
+		nbytes++;
+	}
+	for(wbytes=0;nbytes>0;)
+	{
+		int rw,len;
+		rw=write(STDOUT_FILENO,&buf[wbytes],nbytes);
+		ntime++;
+		if(rw<0)
+		{
+			error++;
+			sprintf(rebuf,error_message,ntime,error);
+			len=strlen(rebuf);
+			write(fd,rebuf,len*sizeof(char));
+		}
+		else
+		{
+			success++;
+			nbytes-=rw;
+			wbytes+=rw;
+		}
+	}
+	fprintf(stderr,"error %d\n",error);
 	return 0;
 }
 
