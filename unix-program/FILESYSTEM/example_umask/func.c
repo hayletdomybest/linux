4a2c8b4edd81137c2a584aef893ed5d23a0567d7
t628x7600 <t628x7600@gmail.com>
2017-12-18 11:55:38 +0800

    modify

--- unix-program/FILESYSTEM/example_umask/func.c
+++ unix-program/FILESYSTEM/example_umask/func.c
@@ -1,15 +1,15 @@
 #include "common.h"
 #include <string.h>
 const char* param[]={\
-					"---","--x","-w-","-wx"\
-					,"r--","r-x","rw-","rwx"\
-					};
+		"---","--x","-w-","-wx"\
+		,"r--","r-x","rw-","rwx"\
+		};
 struct stat* readstat(const char* path)
 {
 	struct stat* sbuf=(struct stat*)malloc(sizeof(struct stat));
 	if((stat(path,sbuf))<0)
 	{
-		perror("read stat error\n";
+		perror("read stat error\n");
 		return NULL;
 	}
 	return sbuf;
@@ -18,11 +18,12 @@ struct stat* readstat(const char* path)
 void readpermission(mode_t mode)
 {
 	int i,ref;
-	char ans[8];
+	char* ans=calloc(8,sizeof(char));
 	for(i=2;i>=0;i--)
 	{
 		ref=(mode>>(i*3))&0x07;
-		strcat(ans,param[mode]);
+		strcat(ans,param[ref]);
 	}
 	fprintf(stderr,"%s\n",ans);
+	free(ans);
 }
